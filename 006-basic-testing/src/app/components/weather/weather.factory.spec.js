bc5af49fc309e6e9f23997a3257525ff8c022179
Luisa Valencia <luisa.valencia@talosdigital.com>
2017-05-24 22:33:03 -0500

    Solving Practice No.006

--- 006-basic-testing/src/app/components/weather/weather.factory.spec.js
+++ 006-basic-testing/src/app/components/weather/weather.factory.spec.js
@@ -2,12 +2,19 @@
   'use strict';
 
   describe('service weatherFactory', function() {
-    var weatherFactory;
+    var weatherFactory, suite;
 
     beforeEach(module('angularApp'));
     beforeEach(module('ngResource'));
+
     beforeEach(inject(function($injector) {
+      suite = {};
       weatherFactory = $injector.get('WeatherFactory');
+      suite.httpBackend = $injector.get('$httpBackend');
+      suite.WEATHER_API = $injector.get('WEATHER_API');
+
+      suite.httpBackend.whenGET(/(.*)\.html/).respond();
+      suite.httpBackend.whenGET(/(.*)\.json/).respond();
     }));
 
 
@@ -19,13 +26,26 @@
       it('should exist', function() {
         expect(weatherFactory.getWeather).not.toEqual(null);
       });
+      it('should make a GET to http://api.openweathermap.org/data/2.5/weather and resolve', function(){
+        var lat = '123', lon = '456';
+        suite.httpBackend.expectGET('http://api.openweathermap.org/data/2.5/weather?APPID='+suite.WEATHER_API+'&lat='+lat+'&lon='+lon).respond(200,{});
+        weatherFactory.getWeather(lat, lon).query().$promise.then(function (response){
+          expect(response).toEqual({});
+        });
+      });
     });
 
     describe('getUV function', function() {
       it('should exist', function() {
         expect(weatherFactory.getUV).not.toEqual(null);
       });
+      it('should make a GET to http://api.openweathermap.org/data/2.5/weather and resolve', function(){
+        var lat = '123', lon = '456';
+        suite.httpBackend.expectGET('http://api.openweathermap.org/v3/uvi/:latlng/current.json?appid='+suite.WEATHER_API).respond(200,{});
+        weatherFactory.getUV().query({latlng: [lat, lon]}).$promise.then(function (response){
+          expect(response).toEqual({});
+        });
+      });
     });
-
   });
 })();
