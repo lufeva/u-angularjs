ee1a9d14462deb4ca2b8f053814404e1a1f101a5
Luisa Valencia <luisa.valencia@talosdigital.com>
2017-06-03 22:48:25 -0500

    Adding Directive’s Unit Testing, Fixing problem in Controller’s Unit testing

--- 008-testing-directives/src/app/components/card/card.directive.spec.js
+++ 008-testing-directives/src/app/components/card/card.directive.spec.js
@@ -1,13 +1,84 @@
 (function() {
   'use strict';
 
+  var elm, $compile, $rootScope;
+
   describe('directive card', function() {
-   
+
     beforeEach(module('angularApp'));
 
     beforeEach(inject(function ($injector) {
-	  /* ... */
-	}));
+    	$rootScope = $injector.get('$rootScope');
+    	$compile = $injector.get('$compile');
 
+    	$rootScope.cardValues = {
+    		title: '',
+    		description : '',
+    		background : '',
+    		textColor: '',
+    		reservedBy: '',
+    		icon: 'iconMock',
+    		setFavorite : function() {},
+    	};
+
+    	elm = angular.element(
+    		'<card title="cardValues.title"' +
+	            'description="cardValues.description"' +
+	            'background="cardValues.background"' +
+	            'text-color="cardValues.textColor"' +
+	            'reserved-by="{{cardValues.reservedBy}}"' +
+	            'cb-favorite="cardValues.setFavorite(title)"' +
+	            'icon="cardValues.icon">' +
+      		'</card>'
+    	);
+
+	    $compile(elm)($rootScope);
+    	$rootScope.$digest();
+  	}));
+
+  	it('Should render cardValues.title into card directive', function () {
+  		var titleElement = elm.find('.title');
+  		$rootScope.cardValues.title = 'titleMock';
+        $rootScope.$digest();
+
+        expect(titleElement).toBeDefined();
+        expect(titleElement.text()).toEqual($rootScope.cardValues.title);
+    });
+
+    it('Should render cardValues.description into card directive', function () {
+  		var descriptionElement = elm.find('.description');
+  		$rootScope.cardValues.description = 'descriptionMock';
+        $rootScope.$digest();
+
+        expect(descriptionElement).toBeDefined();
+        expect(descriptionElement.text()).toEqual($rootScope.cardValues.description);
+    });
+
+    it('Should render cardValues.reservedBy into card directive', function () {
+  		var reservedByElement = elm.find('.reservedBy');
+  		$rootScope.cardValues.reservedBy = 'reservedByMock';
+        $rootScope.$digest();
+
+        expect(reservedByElement).toBeDefined();
+        expect(reservedByElement.text()).toEqual($rootScope.cardValues.reservedBy);
+    });
+
+    it('Should render cardValues.textColor into card directive', function () {
+  		var descriptionElement = elm.find('.card');
+  		$rootScope.cardValues.textColor = '#333';
+        $rootScope.$digest();
+
+        expect(descriptionElement).toBeDefined();
+        expect(descriptionElement.css('color')).toEqual($rootScope.cardValues.textColor);
+    });
+
+    it('Should render cardValues.background into card directive', function () {
+  		var descriptionElement = elm.find('.card');
+  		$rootScope.cardValues.background = '#FFF';
+        $rootScope.$digest();
+
+        expect(descriptionElement).toBeDefined();
+        expect(descriptionElement.css('background-color')).toEqual($rootScope.cardValues.background);
+    });
   });
 })();
--- 008-testing-directives/src/app/states/home/home.controller.spec.js
+++ 008-testing-directives/src/app/states/home/home.controller.spec.js
@@ -8,6 +8,11 @@
 
     beforeEach(inject(function($injector) {
       suite.$controller = $injector.get('$controller');
+      suite.$rootScope = $injector.get('$rootScope');
+
+      suite.vm = suite.$controller('HomeController', {
+        $rootScope: suite.$rootScope
+      });
     }));
 
     it('should be registered', function() {
